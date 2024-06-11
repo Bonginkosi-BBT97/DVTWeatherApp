0b6c6c771747c2cd0e5148b3e1a8cc0182dd12fe
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-11 18:33:17 +0200

    added get current weather  function into the Weather service class

--- DVTWeatherApp/DVTWeatherApp/Networking /Service/WeatherAPIService.swift
+++ DVTWeatherApp/DVTWeatherApp/Networking /Service/WeatherAPIService.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+class WeatherAPIService {
+  private let session: URLSessionProtocol
+  private let apiToken = "f77ddc0a24c1b09b6e4f6bd66e3bc0ab"
+  init(session: URLSessionProtocol = URLSession.shared) {
+    self.session = session
+  }
+
+  func getCurrentWeather(city: String) async throws -> ResponseBody {
+    guard let url =
+      URL(
+        string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiToken)&units=metric"
+      )
+    else {
+      fatalError("the URL is missing")
+    }
+
+    let urlRequest = URLRequest(url: url)
+
+    let (data, response) = try await session.data(for: urlRequest)
+
+    guard let httpResponse = response as? HTTPURLResponse else {
+      throw WeatherAPIError.invalidResponse
+    }
+
+    guard httpResponse.statusCode == 200 else {
+      throw WeatherAPIError.failedRequest(statusCode: httpResponse.statusCode)
+    }
+    do {
+      let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+      return decodedData
+    } catch {
+      throw WeatherAPIError.invalidData
+    }
+  }
+}
