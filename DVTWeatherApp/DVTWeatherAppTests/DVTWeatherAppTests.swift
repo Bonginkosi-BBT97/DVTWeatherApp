687073081299f3618194dd0571ec1b02e547a32f
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-11 18:09:33 +0200

    Added enum model for weather errors

--- DVTWeatherApp/DVTWeatherAppTests/DVTWeatherAppTests.swift
+++ DVTWeatherApp/DVTWeatherAppTests/DVTWeatherAppTests.swift
@@ -5,30 +5,29 @@
 //  Created by Bonginkosi Tshabalala on 2024/06/11.
 //
 
-import XCTest
 @testable import DVTWeatherApp
+import XCTest
 
 final class DVTWeatherAppTests: XCTestCase {
-    
- let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f77ddc0a24c1b09b6e4f6bd66e3bc0ab&units=metric"
- let cityName = "London"
-    
-    func testGetCurrentWeather_Success() async throws {
-        //GIVEN
-        //WHEN
-        //THEN
-    }
-    
-    func testGetCurrentWeather_Error() async throws {
-        //GIVEN
-        //WHEN
-        //THEN
-    }
-    
-    func testGetCurrentWeather_InvalidData() async throws {
-        //GIVEN
-        //WHEN
-        //THEN
-    }
-    
+  let currentWeatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f77ddc0a24c1b09b6e4f6bd66e3bc0ab&units=metric"
+  let cityName = "London"
+
+  func testGetCurrentWeather_Success() async throws {
+    // GIVEN
+    // WHEN
+    // THEN
+  }
+
+  func testGetCurrentWeather_Error() async throws {
+    // GIVEN
+    // WHEN
+    // THEN
+  }
+
+  func testGetCurrentWeather_InvalidData() async throws {
+    // GIVEN
+    // WHEN
+    // THEN
+  }
 }
