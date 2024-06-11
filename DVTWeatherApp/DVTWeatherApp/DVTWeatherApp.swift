09ed0a85bf7ef7626db9271c7b3d5a0bf38b7684
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-11 13:45:03 +0200

    Initial Project files

---
+++ DVTWeatherApp/DVTWeatherApp/DVTWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVTWeatherAppApp.swift
+//  DVTWeatherApp
+//
+//  Created by Bonginkosi Tshabalala on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct DVTWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
