f2e8206873bdea87e4d564b92c4a840eba87ec77
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-14 01:49:10 +0200

    Just improved the appearance of the UITabbar

--- DVTWeatherApp/DVTWeatherApp/Core/DVTWeatherApp.swift
+++ DVTWeatherApp/DVTWeatherApp/Core/DVTWeatherApp.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct DVTWeatherApp: App {
+  init() {
+    UITabBar.appearance().backgroundColor = UIColor.systemBackground
+    UITabBar.appearance().barTintColor = UIColor.white
+    UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+    UITabBar.appearance().tintColor = UIColor.white
+  }
+
   @StateObject var locationManager = LocationManager()
   var body: some Scene {
     WindowGroup {
