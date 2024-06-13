5c00f8c8faf8b2b78fac1b9535834f181811e96a
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-13 10:30:04 +0200

    Passing that from the viewmodel to the view using prints

--- DVTWeatherApp/DVTWeatherApp/Features/HomeTab/View/HomeTabView.swift
+++ DVTWeatherApp/DVTWeatherApp/Features/HomeTab/View/HomeTabView.swift
@@ -5,10 +5,17 @@
 //  Created by Bonginkosi Tshabalala on 2024/06/12.
 //
 
+import Combine
+import CoreLocation
 import SwiftUI
 
 struct HomeTabView: View {
-  let currentTemperature: String
+  @EnvironmentObject var locationManager: LocationManager
+  @StateObject var homeTabViewModel = HomeTabViewModel()
+  @State private var cancellables: Set<AnyCancellable> = []
+
+  @State var currentTemperature: String
+
   let weatherTitle: String
   let backgroundImageName: String
   let backgroundColor: Color
@@ -19,15 +26,29 @@ struct HomeTabView: View {
       CustomDividerView()
       middleSection
       bottomSection
+        .onAppear {}
       Spacer()
     }
     .background(backgroundColor)
+    .onAppear {
+      homeTabViewModel.fetchWeather(for: locationManager.location!)
+      homeTabViewModel.$weather
+        .sink { weather in
+          if let currentWeather = weather {
+            currentTemperature = String(currentWeather.main.temp)
+            print("Temperature: \(currentWeather.main.temp)")
+            print("Weather: \(currentWeather.weather.first?.description ?? "")")
+            print("City Name: \(currentWeather.name)")
+          }
+        }
+        .store(in: &cancellables)
+    }
   }
 }
 
 #Preview {
   HomeTabView(
-    currentTemperature: "25",
+    homeTabViewModel: HomeTabViewModel(), currentTemperature: "25",
     weatherTitle: "SUNNY",
     backgroundImageName: "sunny",
     backgroundColor: Color.green
