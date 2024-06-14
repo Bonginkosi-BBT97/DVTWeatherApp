f3330c9fa22dadbb102543f40fb35b1222a8b245
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-14 05:39:58 +0200

    The UIController that handles the UIKIT table view for the swiftui tabview

---
+++ DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesDetailedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesDetailedViewController.swift
+//  DVTWeatherApp
+//
+//  Created by Bonginkosi Tshabalala on 2024/06/14.
+//
+
+import Foundation
+import UIKit
+
+class FavouritesDetailedViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    title = "Detail"
+  }
+}
