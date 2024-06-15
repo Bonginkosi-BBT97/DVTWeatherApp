60f23d11288971e1aabfa8968676eecceb301d11
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-15 12:05:22 +0200

    Moved the tabke functions to the extension

--- DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesTableView/FavouritesTableViewController+Extension.swift
+++ DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesTableView/FavouritesTableViewController+Extension.swift
@@ -6,5 +6,44 @@
 //
 
 import Foundation
+import UIKit
 
-extension FavouritesTableViewController {}
+extension FavouritesTableViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView
+      .dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FavouriteTableViewCell
+    else {
+      return UITableViewCell()
+    }
+
+    let item = items[indexPath.row].components(separatedBy: " ")
+    let cityName = item[0]
+    let temperature = item[1]
+    let weatherDescription = item[2]
+
+    cell.cityName.text = cityName
+    cell.weatherTemperature.text = temperature
+    cell.weatherDescription.text = weatherDescription
+    cell.backgroundColor = .white
+
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+
+    let detailViewController = FavouritesDetailedViewController()
+    navigationController?.pushViewController(detailViewController, animated: true)
+  }
+
+  @objc func addButtonTapped() {
+    let searchViewController = FavouritesSearchViewController()
+    searchViewController.modalPresentationStyle = .popover
+    searchViewController.modalTransitionStyle = .coverVertical
+    present(searchViewController, animated: true, completion: nil)
+  }
+}
--- DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesTableView/FavouritesTableViewController.swift
+++ DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesTableView/FavouritesTableViewController.swift
@@ -9,15 +9,12 @@ import UIKit
 
 class FavouritesTableViewController: UITableViewController {
   var items = ["Johannesburg 12° CLEAR", "Durban 15° CLOUDY", "Pretoria 20° SUNNY "]
-  let viewModel = FavouritesViewModel()
+  var favouritesViewModel = FavouritesViewModel()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    print(viewModel.container.name)
-    let nib = UINib(nibName: "FavouriteTableViewCell", bundle: nil)
-    tableView.register(nib, forCellReuseIdentifier: "Cell")
-    tableView.backgroundColor = .white
+    registerXib()
 
     navigationItem.rightBarButtonItem = UIBarButtonItem(
       title: "Add",
@@ -27,41 +24,9 @@ class FavouritesTableViewController: UITableViewController {
     )
   }
 
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
-  }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView
-      .dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FavouriteTableViewCell
-    else {
-      return UITableViewCell()
-    }
-
-    let item = items[indexPath.row].components(separatedBy: " ")
-    let cityName = item[0]
-    let temperature = item[1]
-    let weatherDescription = item[2]
-
-    cell.cityName.text = cityName
-    cell.weatherTemperature.text = temperature
-    cell.weatherDescription.text = weatherDescription
-    cell.backgroundColor = .white
-
-    return cell
-  }
-
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-
-    let detailViewController = FavouritesDetailedViewController()
-    navigationController?.pushViewController(detailViewController, animated: true)
-  }
-
-  @objc func addButtonTapped() {
-    let searchViewController = FavouritesSearchViewController()
-    searchViewController.modalPresentationStyle = .popover
-    searchViewController.modalTransitionStyle = .coverVertical
-    present(searchViewController, animated: true, completion: nil)
+  private func registerXib() {
+    let nib = UINib(nibName: "FavouriteTableViewCell", bundle: nil)
+    tableView.register(nib, forCellReuseIdentifier: "Cell")
+    tableView.backgroundColor = .white
   }
 }
