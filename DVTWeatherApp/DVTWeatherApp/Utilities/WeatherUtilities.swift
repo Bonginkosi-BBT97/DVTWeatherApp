7ff58c9a46471244a59248e167338e0c7752f692
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-17 01:14:46 +0200

    Created a weather utilities struct
    So I decided to create this file because I need to use some functions that already exists within the hom,e tab view model.  What I need to do is to  extract these functions into this shared class so I do not repeat my self

---
+++ DVTWeatherApp/DVTWeatherApp/Utilities/WeatherUtilities.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherUtilities.swift
+//  DVTWeatherApp
+//
+//  Created by Bonginkosi Tshabalala on 2024/06/17.
+//
+
+import Foundation
+
+struct WeatherUtilities {
+    static func roundTemperatureString(from temperature: Double) -> String {
+        return ""
+    }
+    static func getDayWeek(from dateString: String) -> String {
+        return ""
+    }
+    
+    static func updateWeatherForecast() {
+        
+    }
+}
