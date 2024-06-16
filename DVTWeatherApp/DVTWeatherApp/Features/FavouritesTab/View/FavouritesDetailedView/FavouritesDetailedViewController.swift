0f865c81a6a6f099529b174c06c75055f9704e39
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-16 13:18:36 +0200

    Created the top section for detailed view in storyboard

--- DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesDetailedView/FavouritesDetailedViewController.swift
+++ DVTWeatherApp/DVTWeatherApp/Features/FavouritesTab/View/FavouritesDetailedView/FavouritesDetailedViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class FavouritesDetailedViewController: UIViewController {
   @IBOutlet var heading: UILabel!
-
+  @IBOutlet var currentTemperatureLabel: UILabel!
+  @IBOutlet var currentWeatherDescriptionLabel: UILabel!
+  @IBOutlet var currentTempLabel: UILabel!
   @IBOutlet var changeText: UILabel!
+  @IBOutlet var maxTempLabel: UILabel!
+  @IBOutlet var minTempLabel: UILabel!
+  @IBOutlet var backgroundImage: UIImageView!
 
+  var background: UIColor?
   var changeTextValue: String?
-
+  var currentTempValue: String?
+  var minValue: String?
+  var maxValue: String?
+  var cityName: String?
   override func viewDidLoad() {
     super.viewDidLoad()
     //  view.backgroundColor = .white
-    title = "Detail"
     heading.text = "This heading"
     changeText.text = changeTextValue ?? "Change This"
+    loadData()
+  }
+
+  func loadData() {
+    title = cityName ?? "Weather"
+    currentTemperatureLabel.text = currentTempValue ?? "10"
+    maxTempLabel.text = maxValue ?? "18"
+    minTempLabel.text = minValue ?? "13"
   }
 }
