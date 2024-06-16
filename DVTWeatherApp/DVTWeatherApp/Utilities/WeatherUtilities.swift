6e8e1eeb462a4a2cdd4a82e431f444bf662a5985
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-17 01:41:43 +0200

    Added the getDayOfWeek function to the enum, up next will be LARGE updateWetherForecast function. Will need to find a way to improve this function

--- DVTWeatherApp/DVTWeatherApp/Utilities/WeatherUtilities.swift
+++ DVTWeatherApp/DVTWeatherApp/Utilities/WeatherUtilities.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 enum WeatherUtilities {
-  static func roundTemperatureString(from temperature: Double) -> String {
-    let roundedTemperature = Int(round(temperature))
-    return "\(roundedTemperature)"
-  }
-
-  static func getDayWeek(from dateString: String) -> String {
-    return ""
-  }
-
-  static func updateWeatherForecast() {}
+    static func roundTemperatureString(from temperature: Double) -> String {
+        let roundedTemperature = Int(round(temperature))
+        return "\(roundedTemperature)"
+    }
+    
+    static func getDayOfWeek(from dateString: String) -> String? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let date = formatter.date(from: dateString) else { return nil }
+        formatter.dateFormat = "EEEE"
+        return formatter.string(from: date)
+    }
+    static func updateWeatherForecast() {}
 }
