8f1bb200be10da38eec540f27019e35e29740dad
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-06-17 02:09:06 +0200

    Updated HomeTabViewModel to use the utilities  enum

--- DVTWeatherApp/DVTWeatherApp/Features/HomeTab/ViewModel/HomeTabViewModel.swift
+++ DVTWeatherApp/DVTWeatherApp/Features/HomeTab/ViewModel/HomeTabViewModel.swift
@@ -54,7 +54,13 @@ class HomeTabViewModel: ObservableObject {
           lon: lon
         )
         self.weatherForecast = weatherData
-        self.updateWeatherForecast(forecastResponse: weatherData)
+        WeatherUtilities.updateWeatherForecast(
+          forecastResponse: weatherData,
+          forecastDaysOfWeek: &forecastDaysOfWeek,
+          forecastTemperatures: &forecastTemperatures,
+          forecastDescriptions: &forecastDescriptions,
+          forecastIconNames: &forecastIconNames
+        )
       } catch {
         self.errorMessage = error.localizedDescription
       }
@@ -83,60 +89,5 @@ class HomeTabViewModel: ObservableObject {
 
     let backgroundColor = BackgroundColorCode(imageName: backgroundImage)
     self.backgroundColor = backgroundColor.color
-  }
-
-  func getDayOfWeek(from dateString: String) -> String? {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    guard let date = formatter.date(from: dateString) else { return nil }
-    formatter.dateFormat = "EEEE"
-    return formatter.string(from: date)
-  }
-
-  private func updateWeatherForecast(forecastResponse: WeatherForecastResponse) {
-    var uniqueDays = Set<String>()
-    var printedDaysCount = 0
-
-    let forecastDetails = forecastResponse.list.map { forecast -> (
-      day: String,
-      temperature: String,
-      description: String
-    ) in
-      let day = getDayOfWeek(from: forecast.dtTxt) ?? "Week Day"
-      let temperature = String(format: "%.2f", forecast.main.temp)
-      let description = forecast.weather.first?.main.description ?? "Description"
-      return (day, temperature, description)
-    }
-
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd"
-    let todayDateString = dateFormatter.string(from: Date())
-    let today = getDayOfWeek(from: "\(todayDateString) 00:00:00") ?? "Week Day"
-
-    for detail in forecastDetails {
-      if printedDaysCount >= 5 {
-        break
-      }
-
-      if detail.day == today {
-        continue
-      }
-
-      if !uniqueDays.contains(detail.day) {
-        forecastDaysOfWeek.append(detail.day)
-        forecastTemperatures.append(WeatherUtilities.roundTemperatureString(from: Double(detail.temperature) ?? 0.0))
-        forecastDescriptions.append(detail.description)
-
-        if let weatherDescription = WeatherDescription(rawValue: detail.description.uppercased()) {
-          let iconName = ForecastIconNameEnum(description: weatherDescription).rawValue
-          forecastIconNames.append(iconName)
-        } else {
-          forecastIconNames.append(ForecastIconNameEnum(description: .unknown).rawValue)
-        }
-
-        uniqueDays.insert(detail.day)
-        printedDaysCount += 1
-      }
-    }
   }
 }
